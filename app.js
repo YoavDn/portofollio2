3382e6a941ab9f5a36da5c14ec6ae06eb9308772
yoav <yoavdan.m@gmail.com>
2022-05-01 19:16:13 +0300

    new shit

--- app.js
+++ app.js
@@ -4,39 +4,12 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const letsTalkBtn = document.querySelector('.lets-talk-btn');
 const btnCloseModal = document.querySelector('.btn-close-modal');
+const homeText = document.querySelector('.main-text-plus-btn');
 
-const openModal = function (e) {
-  e.preventDefault();
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
-};
-const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-};
-letsTalkBtn.addEventListener('click', openModal);
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
-
-//// revel sections
-
-const allSections = document.querySelectorAll('.section');
-
-const revealSection = function (entries, observer) {
-  const [entry] = entries;
-
-  if (!entry.isIntersecting) return;
-
-  entry.target.classList.remove('section--hidden');
-  observer.unobserve(entry.target);
+const showTitle = function (e) {
+  e.prevent.default();
+  homeText.classList.remove('hidden');
+  console.log('should work');
 };
 
-const sectionObserver = new IntersectionObserver(revealSection, {
-  root: null,
-  threshold: 0.15,
-});
-
-allSections.forEach(function (section) {
-  sectionObserver.observe(section);
-  section.classList.add('section--hidden');
-});
+showTitle(e);
